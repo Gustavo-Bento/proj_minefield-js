910bc38a6d10ade91cc56e1c2eaef7c08a060e4b
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-28 10:41:07 -0300

    Styling the minefields

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,28 +1,52 @@
-import React from 'react'
-import {View, StyleSheet} from 'react-native'
-import params from '../Params'
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import params from '../Params';
 
-export default props =>{
-  const styleField = [style.field]
-  //Outros estilos aqui
-  if (styleField.length ===1) styleField.push(style.regular)
+export default (props) => {
+  const { mined, opened, nearMines } = props;
 
-  return(
-    <View style = {styleField}></View>
+  const styleField = [style.field];
+  if (opened) styleField.push(style.opened);
+  if (styleField.length === 1) styleField.push(style.regular);
+
+  let color = null
+  if (nearMines > 0) {
+    if (nearMines == 1) color = '#2A28D7';
+    if (nearMines == 2) color = '#28520F';
+    if (nearMines > 2 && nearMines < 6) color = '#F9060A';
+    if (nearMines >= 6) color = '#F221A9';
+  }
+
+  return (
+    <View style={styleField}>
+      {!mined&& opened && nearMines > 0 ? 
+      <Text style={[style.label, { color: color }]}>{nearMines}</Text>: false}
+    </View>
   )
-}
+  
+};
 
 const style = StyleSheet.create({
-  field:{
+  field: {
     height: params.blockSize,
     width: params.blockSize,
     borderWidth: params.borderSize,
   },
-  regular:{
+  regular: {
     backgroundColor: '#999',
     borderLeftColor: '#CCC',
     borderTopColor: '#CCC',
-    bordrerRightColor: '#333',
+    borderRightColor: '#333',
     borderBottomColor: '#333',
+  },
+  opened:{
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
   }
-})
+});
