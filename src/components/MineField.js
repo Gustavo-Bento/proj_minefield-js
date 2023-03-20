ad8a1f3cafc0b50a66e29a5c1f02507e164ccdff
Gustavo Ferreira <gfmb94@gmail.com>
2023-03-20 17:52:19 -0300

    Update minefields style

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -7,14 +7,13 @@ export default props => {
     const columns = row.map((field, c) => {
       return <Field {...field} key={c} />
     })
-    return <View key={r}>{columns}</View>
+    return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
   })
   return <View style={style.container}>{rows}</View>
 }
 
 const style = StyleSheet.create({
   container:{
-    flexDirection:'row',
     backgroundColor: '#EEE',
   }
 })
