c0f325ac5fc2894a698ea73c625b8dadbb0b056e
Gustavo Ferreira <gfmb94@gmail.com>
2023-03-27 09:33:15 -0300

    Implementing onpress

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Params from './src/Params';
 import MineField from './src/components/MineField'
 import {
-  createMinedBoard
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines
 } from './src/Functions'
 
 export default class App extends Component {
@@ -23,8 +28,26 @@ export default class App extends Component {
     const rows = Params.getRowsAmount()
     return {
       board: createMinedBoard(rows,cols, this.minesAmount()),
+      won: false,
+      lost: false
       
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if(lost){
+      showMines(board)
+      Alert.alert('💣 Booom:', '🪦 Você morreu!')
+    }
+    if(won){
+      Alert.alert('🏆 Parabéns','😎 você ganhou!')
+    }
+    this.setState({board, lost, won})
   }
 
   render() {
@@ -36,7 +59,8 @@ export default class App extends Component {
           {Params.getRowsAmount()}x{Params.getColumnsAmount()}
         </Text>
         <View style={style.board}>
-          <MineField board={this.state.board} />
+          <MineField board={this.state.board} 
+          onOpenField = {this.onOpenField}/>
         </View>
       </View>
     );
